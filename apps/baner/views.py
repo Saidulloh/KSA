69ef1850f10b39096e6c89924252e1bf1e4367e0
Saidulloh <ssavutikhunov@gmail.com>
2024-03-26 06:11:59 +0600

    create mixin for views, get only last object(baner)

--- apps/baner/views.py
+++ apps/baner/views.py
@@ -1,19 +1,29 @@
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
 from rest_framework.mixins import ListModelMixin, RetrieveModelMixin
 from rest_framework.permissions import IsAdminUser
+from rest_framework.response import Response
 
 from apps.baner.models import Baner
 from apps.baner.serializers import BanerSerializer
 
 
-class AdminBanerApiViewSet(ModelViewSet):
+class ListMixin(ListModelMixin):
+    # function for getting last baner
+    def list(self, request, *args, **kwargs):
+        queryset = Baner.objects.all().order_by('-id')[:1]
+        serializer = BanerSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+
+class AdminBanerApiViewSet(ModelViewSet,
+                           ListMixin):
     queryset = Baner.objects.all()
     serializer_class = BanerSerializer
     permission_classes = [IsAdminUser]
 
 
 class BanerApiViewSet(GenericViewSet,
-                           ListModelMixin,
-                           RetrieveModelMixin):
+                      ListMixin,
+                      RetrieveModelMixin):
     queryset = Baner.objects.all()
     serializer_class = BanerSerializer
